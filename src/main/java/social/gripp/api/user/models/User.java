460f0dab467e57129a1302a89c9a76bd877219ab
matt62king <matt62king@gmail.com>
2018-09-09 14:59:35 -0500

    Add Keys to data models

--- src/main/java/social/gripp/api/user/models/User.java
+++ src/main/java/social/gripp/api/user/models/User.java
@@ -1,14 +1,22 @@
 package social.gripp.api.user.models;
 
+import com.google.cloud.datastore.Key;
+
 import java.util.List;
 
 public interface User {
+    Key getKey();
+    void setKey(Key key);
+
     String getHandle();
     void setHandle(String handle);
+
     String getEmail();
     void setEmail(String email);
+
     String getPassword();
     void setPassword(String password);
+
     List<UserAuthority> getUserAuthorities();
     void setUserAuthorities(List<UserAuthority> userAuthorities);
 }
--- src/main/java/social/gripp/api/user/models/UserAuthority.java
+++ src/main/java/social/gripp/api/user/models/UserAuthority.java
@@ -1,10 +1,15 @@
 package social.gripp.api.user.models;
 
+import com.google.cloud.datastore.Key;
 import social.gripp.api.constraints.Authority;
 
 public interface UserAuthority {
+    Key getKey();
+    void setKey(Key key);
+
     String getUserEmail();
     void setUserEmail(String userEmail);
+
     Authority getUserAuthority();
     void setUserAuthority(Authority userAuthority);
 }
