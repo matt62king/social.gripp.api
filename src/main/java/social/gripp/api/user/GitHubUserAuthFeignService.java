8c650bf3b7024854466ac065490e1f393f80111b
matt62king <matt62king@gmail.com>
2018-11-01 22:53:12 -0500

    updated method

--- src/main/java/social/gripp/api/user/GitHubUserAuthFeignService.java
+++ src/main/java/social/gripp/api/user/GitHubUserAuthFeignService.java
@@ -2,13 +2,13 @@ package social.gripp.api.user;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import social.gripp.api.user.models.GitHubUserAuth;
 
 @FeignClient(value = "auth-server")
 public interface GitHubUserAuthFeignService {
 
-    @PutMapping("api/v1/user/proxy/github/token")
+    @PostMapping("api/v1/user/proxy/github/token")
     ResponseEntity<GitHubUserAuth> getGitHubToken(@RequestBody GitHubUserAuth gitHubUserAuth);
 }
