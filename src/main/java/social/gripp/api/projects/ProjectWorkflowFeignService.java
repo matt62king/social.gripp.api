3c154b37f6aabc8b9e9975f060d567954aa68aab
matt62king <matt62king@gmail.com>
2018-12-18 06:03:38 -0600

    added workflow endpoints

--- src/main/java/social/gripp/api/projects/ProjectWorkflowFeignService.java
+++ src/main/java/social/gripp/api/projects/ProjectWorkflowFeignService.java
@@ -3,7 +3,6 @@ package social.gripp.api.projects;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -11,13 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import social.gripp.api.projects.models.ProjectWorkflow;
 import social.gripp.api.utils.responces.BooleanResponse;
 
-import java.util.List;
-
 @FeignClient(value = "projects-server")
 public interface ProjectWorkflowFeignService {
-
-    @GetMapping("api/v1/project/details/workflow/{id}")
-    ResponseEntity<List<ProjectWorkflow>> getWorkflowsForProject(@PathVariable("id") String id);
 
     @PostMapping("api/v1/project/details/workflow/create")
     ResponseEntity<ProjectWorkflow> createWorkflow(@RequestBody ProjectWorkflow projectWorkflow);
