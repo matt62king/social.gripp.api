54fbcec8aa58850e5dbc68a227ef12dfe1f38749
matt62king <matt62king@gmail.com>
2019-01-05 15:58:28 -0600

    updated duration code enum

--- src/main/java/social/gripp/api/global/models/DurationUnit.java
+++ src/main/java/social/gripp/api/global/models/DurationUnit.java
@@ -1,5 +1,6 @@
 package social.gripp.api.global.models;
 
+import com.google.common.collect.Lists;
 import social.gripp.api.utils.CodedEnum;
 
 public enum DurationUnit implements CodedEnum {
@@ -16,5 +17,11 @@ public enum DurationUnit implements CodedEnum {
     @Override
     public String getCode() {
         return code;
+    }
+
+    public static DurationUnit enumCode(String code) {
+        return Lists.newArrayList(values()).stream()
+                .filter(durationUnit -> code.equalsIgnoreCase(durationUnit.getCode()))
+                .findFirst().orElseThrow(() -> new RuntimeException("No duration unit for code: " + code));
     }
 }
