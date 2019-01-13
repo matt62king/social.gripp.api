f1c02e62101e325e23adb68b10a5b1912c703d4d
matt62king <matt62king@gmail.com>
2019-01-13 00:02:52 -0600

    added task update endpoints

--- src/main/java/social/gripp/api/tasks/TaskFeignService.java
+++ src/main/java/social/gripp/api/tasks/TaskFeignService.java
@@ -3,12 +3,20 @@ package social.gripp.api.tasks;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import social.gripp.api.tasks.models.Task;
+import social.gripp.api.tasks.models.TaskAssign;
 
 @FeignClient(value = "tasks-server")
 public interface TaskFeignService {
 
     @PostMapping("api/v1/tasks/create")
     ResponseEntity<Task> createTask(@RequestBody Task task);
+
+    @PutMapping("api/v1/task/update")
+    ResponseEntity<Task> updateTask(@RequestBody Task task);
+
+    @PutMapping("api/v1/task/assign")
+    ResponseEntity<Task> assignTask(@RequestBody TaskAssign taskAssign);
 }
